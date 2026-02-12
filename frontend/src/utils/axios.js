7e80b461cce0271aac71042d876f0b42593fdfbc
Vikashhh18 <Vikash.sharma1761@gmail.com>
2026-02-12 15:36:57 +0530

    ready to deploy

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://talkify-cc5m.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
